a971b4cebbb828e6f4893a3758ba216a371f8e27
Jay-sanjay <134289328+Jay-sanjay@users.noreply.github.com>
2024-05-10 00:56:25 +0530

    make update

--- docs/make.jl
+++ docs/make.jl
@@ -4,36 +4,41 @@ using Documenter, DocumenterVitepress
 DocMeta.setdocmeta!(OMOPCDMPathways, :DocTestSetup, :(using OMOPCDMPathways); recursive=true)
 
 pgs=[
-        "Home" => "index.md",
+    "Home" => "index.md",
+    "Tutorials" => [
+                    "tutorials.md",
+                    "tutorials/beginner_tutorial.md",
+                    "tutorials/r_tutorial.md"
+                   ],
+    "API" => "api.md",
+    "Contributing" => "contributing.md"
 ]
 
 fmt  = DocumenterVitepress.MarkdownVitepress(
-    repo="https://github.com/JuliaHealth/OMOPCDMPathways",
+    repo="https://github.com/JuliaHealth/OMOPCDMPathways.jl",
+    devbranch = "main",
     devurl = "dev",
-    # deploy_url = "yourgithubusername.github.io/OMOPCDMPathways.jl",
     build_vitepress = false,
 )
-# )
+
 
 makedocs(;
     modules = [OMOPCDMPathways],
     authors = "Jay-sanjay <landgejay124@gmail.com> and contributors",
-    repo = "https://github.com/JuliaHealth/OMOPCDMPathways",
+    repo="https://github.com/JuliaHealth/OMOPCDMPathways.jl",
     sitename = "OMOPCDMPathways.jl",
     format = fmt,
     pages= pgs,
     warnonly = true,
+    draft = false,
+    source = "src",
+    build = "build"
 )
 
 deploydocs(;
-    repo="https://github.com/JuliaHealth/OMOPCDMPathways",
+    repo="https://github.com/JuliaHealth/OMOPCDMPathways.jl",
     target="build", # this is where Vitepress stores its output
     branch = "gh-pages",
     devbranch = "main",
     push_preview = true,
 )
-
-
-"""
-To build docs locally run ths in the docs folder: npm run docs:dev
-"""
