c549237919afb45cfe26ba60cd37bc13b8774672
Jay-sanjay <134289328+Jay-sanjay@users.noreply.github.com>
2024-05-31 15:45:25 +0530

    Added Eunomia Dataset

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,35 @@
 using OMOPCDMPathways
+using DataFrames
+using Dates
+using FunSQL:
+	From,
+	Fun,
+	Get,
+	Where,
+	Group,
+	Limit,
+	Select,
+	render, 
+	Agg,
+	LeftJoin
+using HealthSampleData
+using OMOPCDMCohortCreator
+using SQLite
 using Test
+using TimeZones
+
+using JSON3
+using OHDSICohortExpressions: translate, Model
+
+import DBInterface as DBI
+
+# For allowing HealthSampleData to always download sample data
+ENV["DATADEPS_ALWAYS_ACCEPT"] = true
+
+# SQLite Data Source
+sqlite_conn = SQLite.DB(Eunomia())
+GenerateDatabaseDetails(:sqlite, "main")
+GenerateTables(sqlite_conn)
 
 @testset "OMOPCDMPathways.jl" begin
     # Write your tests here.
