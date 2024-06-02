f205a7c6c50d35eaae5214cf867c9b29998038c1
Jay-sanjay <134289328+Jay-sanjay@users.noreply.github.com>
2024-06-03 02:55:39 +0530

    added maketable

--- src/OMOPCDMPathways.jl
+++ src/OMOPCDMPathways.jl
@@ -1,6 +1,46 @@
 module OMOPCDMPathways
 
-# Write your package code here.
+using DataFrames
+using Dates
+using DBInterface
+using FunSQL:
+    SQLTable,
+    Agg,
+    As,
+    Define,
+    From,
+    Fun,
+    Get,
+    Group,
+    Join,
+    Order,
+    Select,
+    WithExternal,
+    Where,
+    render,
+    Limit,
+    ID,
+    LeftJoin,
+    reflect
+using TimeZones
+
+
+using OMOPCDMCohortCreator
+
+function MakeTables(conn, dialect, schema)
+
+    @eval global dialect = $(QuoteNode(dialect))
+    GenerateDatabaseDetails(dialect, schema)
+    db_info = GenerateTables(conn, inplace = false, exported = true)
+    for key in keys(db_info)
+        @eval global $(Symbol(lowercase(string(key)))) = $(db_info[key])
+        @info "$(lowercase(string(key))) table generated internally"
+    end
+
+end
+
+export MakeTables
+
 function hello()
   "world"
 end
