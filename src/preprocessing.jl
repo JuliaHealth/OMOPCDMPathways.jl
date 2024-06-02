0a4f1b19877e49de51d3c70e65568b15cb098d3e
Jay-sanjay <134289328+Jay-sanjay@users.noreply.github.com>
2024-06-03 03:00:58 +0530

    dataset testing

---
+++ src/preprocessing.jl
@@ -0,0 +1,18 @@
+using Dates
+
+function Dummy(
+    drug_exposure_ids;
+    tab = drug_exposure
+)
+
+    sql =
+        From(tab) |>
+        Where(Fun.in(Get.drug_exposure_id, drug_exposure_ids...)) |>
+        Select(Get.drug_exposure_id, Get.drug_exposure_start_date) |>
+        q -> render(q, dialect=dialect)
+
+    return String(sql)
+
+end
+
+export Dummy
