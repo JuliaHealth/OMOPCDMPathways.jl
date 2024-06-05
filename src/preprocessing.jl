cf724f0e458eff6bfde2e2d05319f09c59d51971
Jay-sanjay <134289328+Jay-sanjay@users.noreply.github.com>
2024-06-05 23:49:28 +0530

    Added the EraCollapse function

--- src/preprocessing.jl
+++ src/preprocessing.jl
@@ -1,4 +1,4 @@
-using Dates
+using DataFrames, Dates
 
 function Dummy(
     drug_exposure_ids,
@@ -26,4 +26,43 @@ function Dummy(
 
 end
 
-export Dummy
+
+
+"""
+Given a treatment history dataframe, this function collapses eras that are separated by a gap of size <= eraCollapseSize.
+
+# TODO: 
+(1) Need to have a column similar to gap_same in the dataframe.
+
+# Implemetation: 
+(1) Sorts the dataframe by event_start_date and event_end_date.
+(2) Calculates the gap between each era and the previous era.
+(3) Filters out rows with gap_same > eraCollapseSize.
+
+
+# Arguments:
+
+- treatment_history: DataFrame, treatment history dataframe.containing drug_exposure_start and drug_exposure_end
+- eraCollapseSize: Int, maximum gap size for collapsing eras.
+
+# Returns:
+
+- Updated dataframe, eras collapsed based on the specified gap size.
+"""
+
+
+function EraCollapse(treatment_history::DataFrame, eraCollapseSize::Int)
+    sort!(treatment_history, [:drug_exposure_start, :drug_exposure_end])
+    treatment_history.lag_variable = circshift(treatment_history.drug_exposure_end, 1)
+    treatment_history.gap_same = treatment_history.drug_exposure_start - treatment_history.lag_variable
+    
+    treatment_history = filter(row -> row[:gap_same] <= eraCollapseSize, treatment_history)
+    select!(treatment_history, Not(:lag_variable))  
+    
+    @info "After eraCollapseSize: $(nrow(treatment_history))"  # For debugging purposes
+    
+    return treatment_history
+end
+
+
+export Dummy, EraCollapse
--- test/Data-Preprocessing/preprocessing.jl
+++ test/Data-Preprocessing/preprocessing.jl
@@ -11,3 +11,24 @@ using Test
     @test test_drug_exposure_ids == result.drug_exposure_id[1:4]
 
 end
+
+
+
+@testset "Era Collapse Tests" begin
+    MakeTables(sqlite_conn, :sqlite, "main")
+    
+    test_person_ids = [1, 1, 1, 1, 1]
+    test_drug_start_date = [-3.727296e8, 2.90304e7, -5.333472e8, -8.18208e7, 1.3291776e9]
+    test_drug_end_date = [-364953600, 31449600, -532483200, -80006400, 1330387200]
+    test_df3 = DataFrame(person_id = test_person_ids, drug_exposure_start = test_drug_start_date, drug_exposure_end = test_drug_end_date)
+    
+    expected_person_id = [1, 1, 1, 1]
+    expected_drug_exposure_start = [-5.333472e8, -3.727296e8, -8.18208e7, 2.90304e7]
+    expected_drug_exposure_end = [-532483200, -364953600, -80006400, 31449600]
+
+    result = EraCollapse(test_df3, 400000000)
+    
+    @test expected_person_id == result.person_id[1:4]
+    @test expected_drug_exposure_start == result.drug_exposure_start[1:4]
+    @test expected_drug_exposure_end == result.drug_exposure_end[1:4]
+end
