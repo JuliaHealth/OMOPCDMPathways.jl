f90444db5921f3b2b73a162f9ca26ea09d14e193
Jay-sanjay <134289328+Jay-sanjay@users.noreply.github.com>
2024-06-09 00:12:44 +0530

    print

--- test/Data-Preprocessing/preprocessing.jl
+++ test/Data-Preprocessing/preprocessing.jl
@@ -6,7 +6,7 @@ using Test
     test_drug_exposure_start_date = [-3.727296e8, 2.90304e7, -5.333472e8, -8.18208e7]
     test_df1 = DataFrame(drug_exposure_id = test_drug_exposure_ids, drug_exposure_start_date = test_drug_exposure_start_date)
     result =  Dummy(test_drug_exposure_ids, sqlite_conn)
-
+    println(result)
     @test test_drug_exposure_start_date == result.drug_exposure_start_date[1:4]
     @test test_drug_exposure_ids == result.drug_exposure_id[1:4]
     
