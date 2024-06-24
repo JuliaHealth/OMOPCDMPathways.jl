bb3856c84814fbbe3ee635f5ad7280981c90d8ad
Jay-sanjay <134289328+Jay-sanjay@users.noreply.github.com>
2024-06-24 10:37:45 +0530

    Update preprocessing.jl

--- test/Data-Preprocessing/preprocessing.jl
+++ test/Data-Preprocessing/preprocessing.jl
@@ -1,24 +1,5 @@
 using Test
 
-@testset "Dummy Tests" begin
-    MakeTables(sqlite_conn, :sqlite, "main")
-    test_drug_exposure_ids = [1.0, 2.0, 3.0, 4.0]
-    test_drug_exposure_start_date = [-3.727296e8, 2.90304e7, -5.333472e8, -8.18208e7]
-    test_df1 = DataFrame(drug_exposure_id = test_drug_exposure_ids, drug_exposure_start_date = test_drug_exposure_start_date)
-    result =  Dummy(test_drug_exposure_ids, sqlite_conn)
-    println(result)
-    @test test_drug_exposure_start_date == result.drug_exposure_start_date[1:4]
-    @test test_drug_exposure_ids == result.drug_exposure_id[1:4]
-    
-    MakeTables(sqlite_conn, :sqlite, "main")
-    test_cohort_ids = [1.0, 1.0, 1.0, 1.0]
-    # test_df2 = DataFrame(cohort_definition_id = test_cohort_definition_id, cohort_id = test_cohort_ids)
-    df = Dummy2(test_cohort_ids, sqlite_conn)
-    println(first(df, 5))
-end
-
-
-
 @testset "Period Prior to Index Tests" begin
     MakeTables(sqlite_conn, :sqlite, "main")
 
@@ -29,8 +10,6 @@ end
     test_df2 = DataFrame(person_id = test_person_ids, cohort_start_date = test_cohort_start_date)
     
     result = Period_prior_to_index(test_person_ids, sqlite_conn)
-
-    println(result)
 
     @test test_person_ids == result.cohort_definition_id[1:5]
     @test test_subject_ids == result.subject_id[1:5]
