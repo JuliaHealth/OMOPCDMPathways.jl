60461bd637bb1a7b9d95b608bbb044d1b495d640
Jay-sanjay <134289328+Jay-sanjay@users.noreply.github.com>
2024-06-28 11:17:30 +0530

    added the requested changes

--- src/preprocessing.jl
+++ src/preprocessing.jl
@@ -29,16 +29,23 @@ end
 
 
 """
-Given a treatment history dataframe, this function collapses eras that are separated by a gap of size <= eraCollapseSize.
-
-# TODO: 
-(1) Need to have a column similar to gap_same in the dataframe.
+# Example:
+    [test_person_ids = [1, 1, 1, 1, 1]
+    test_drug_start_date = [-3.727296e8, 2.90304e7, -5.333472e8, -8.18208e7, 1.3291776e9]
+    test_drug_end_date = [-364953600, 31449600, -532483200, -80006400, 1330387200]]
+    test_df = DataFrame(person_id = test_person_ids, drug_exposure_start = test_drug_start_date, drug_exposure_end = test_drug_end_date)
+    
+    EraCollapse(
+        treatment_history = test_df,
+        eraCollapseSize = 400000000
+    )
 
 # Implemetation: 
-(1) Sorts the dataframe by event_start_date and event_end_date.
-(2) Calculates the gap between each era and the previous era.
-(3) Filters out rows with gap_same > eraCollapseSize.
+    (1) Sorts the dataframe by event_start_date and event_end_date.
+    (2) Calculates the gap between each era and the previous era.
+    (3) Filters out rows with gap_same > eraCollapseSize.
 
+Given a treatment history dataframe, this function collapses eras that are separated by a gap of size <= eraCollapseSize.
 
 # Arguments:
 
@@ -52,14 +59,13 @@ Given a treatment history dataframe, this function collapses eras that are separ
 
 
 function EraCollapse(treatment_history::DataFrame, eraCollapseSize::Int)
+
     sort!(treatment_history, [:drug_exposure_start, :drug_exposure_end])
     treatment_history.lag_variable = circshift(treatment_history.drug_exposure_end, 1)
     treatment_history.gap_same = treatment_history.drug_exposure_start - treatment_history.lag_variable
     
     treatment_history = filter(row -> row[:gap_same] <= eraCollapseSize, treatment_history)
     select!(treatment_history, Not(:lag_variable))  
-    
-    @info "After eraCollapseSize: $(nrow(treatment_history))"  # For debugging purposes
     
     return treatment_history
 end
