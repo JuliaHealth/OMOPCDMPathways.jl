252547ef7e5b3d88403a7736014545d4515b2698
Jacob S. Zelko <jacobszelko@gmail.com>
2024-06-28 12:48:25 -0400

    Final adjustments for docstring

--- src/preprocessing.jl
+++ src/preprocessing.jl
@@ -28,34 +28,51 @@ end
 
 
 """
-# Example:
+```julia
+calculate_era_duration(
+    treatment_history::DataFrame, 
+    minEraDuration::Real
+)
+```
 
-    test_person_ids = [1, 1, 1, 1, 1]
-    test_drug_start_date = [-3.727296e8, 2.90304e7, -5.333472e8, -8.18208e7, 1.3291776e9]
-    test_drug_end_date = [-364953600, 31449600, -532483200, -80006400, 1330387200]
-    
-    test_df = DataFrame(person_id = test_person_ids, drug_exposure_start = test_drug_start_date, drug_exposure_end = test_drug_end_date)
-    
-    calculate_era_duration(
-        treatment_history = test_df, 
-        minEraDuration = 920000
-    )
-
-# Implemetation: 
-    (1) It filters the treatment_history DataFrame to retain only those rows where the duration between drug_exposure_end and drug_exposure_start is at least minEraDuration.
-
-Given a treatment history dataframe, this function filters out rows where the difference between drug_exposure_start and drug_exposure_end is less than minEraDuration.
+Given a treatment history dataframe, this function filters out rows where the difference between `drug_exposure_start` and `drug_exposure_end` is less than `minEraDuration`.
 
     # Arguments:
 
-    - treatment_history: DataFrame, treatment history dataframe.
-    - minEraDuration: Int, minimum duration of an era.
+    - `treatment_history::DataFrame` - treatment history dataframe.
+    - `minEraDuration::Real` - minimum duration of an era.
 
     # Returns:
-    - Updated dataframe, rows where the difference between drug_exposure_start and drug_exposure_end is less than minEraDuration are filtered out.
 
+    - Updated `DataFrame`, rows where the difference between `drug_exposure_start` and `drug_exposure_end` is less than `minEraDuration` are filtered out.
+
+
+# Note: 
+
+    It filters the treatment history `DataFrame` to retain only those rows where the duration between `drug_exposure_end` and `drug_exposure_start` is at least `minEraDuration`.
+
+# Example:
+
+```julia-repl
+julia> test_person_ids = [1, 1, 1, 1, 1];
+
+julia> test_drug_start_date = [-3.727296e8, 2.90304e7, -5.333472e8, -8.18208e7, 1.3291776e9];
+
+julia> test_drug_end_date = [-364953600, 31449600, -532483200, -80006400, 1330387200];
+
+julia> test_df = DataFrame(person_id = test_person_ids, drug_exposure_start = test_drug_start_date, drug_exposure_end = test_drug_end_date);
+
+julia> calculate_era_duration(test_df, 920000)
+4×3 DataFrame
+ Row │ person_id  drug_exposure_start  drug_exposure_end 
+     │ Int64      Float64              Int64             
+─────┼───────────────────────────────────────────────────
+   1 │         1           -3.7273e8          -364953600
+   2 │         1            2.90304e7           31449600
+   3 │         1           -8.18208e7          -80006400
+   4 │         1            1.32918e9         1330387200
+```
 """
-
 function calculate_era_duration(treatment_history::DataFrame, minEraDuration)
     
     treatment_history = filter(row -> (row[:drug_exposure_end] - row[:drug_exposure_start]) >= minEraDuration, treatment_history)    
