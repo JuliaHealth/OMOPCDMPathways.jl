f886d8b1c6daa7dc74883528502cc001dbc19c2e
Jay-sanjay <134289328+Jay-sanjay@users.noreply.github.com>
2024-08-27 23:54:12 +0530

    Update preprocessing.jl

--- src/preprocessing.jl
+++ src/preprocessing.jl
@@ -254,50 +254,29 @@ function create_treatment_history(current_cohorts::DataFrame, targetCohortId::In
     # Add index year column based on start date of target cohort
     targetCohort = current_cohorts[in.(current_cohorts.cohort_id, Ref([targetCohortId])), :]
     targetCohort.index_year = year.(targetCohort.cohort_start_date)
-    #println("Target Cohort DataFrame:")
-    #println(targetCohort)
 
     # Select event cohorts for target cohort and merge with start/end date and index year
     eventCohorts = current_cohorts[in.(current_cohorts.cohort_id, Ref(cohort_ids)), :]
-    #println("\nEvent Cohorts DataFrame:")
-    #println(eventCohorts)
 
     current_cohorts = innerjoin(eventCohorts, targetCohort, on = :subject_id, makeunique = true)
-    #println("\nMerged DataFrame:")
-    #println(current_cohorts)
 
     # Only keep event cohorts starting (startDate) or ending (endDate) after target cohort start date
     if includeTreatments == "startDate"
-        #println("Applying startDate filtering")
-        #println("periodPriorToIndex: ", periodPriorToIndex)
-        #println("cohort_start_date_1: ", current_cohorts.cohort_start_date_1)
-        #("cohort_start_date: ", current_cohorts.cohort_start_date)
-        #println("cohort_end_date_1: ", current_cohorts.cohort_end_date_1)
-
         current_cohorts = current_cohorts[
             (current_cohorts.cohort_start_date_1 .- (periodPriorToIndex) .<= current_cohorts.cohort_start_date) .& 
             (current_cohorts.cohort_start_date .< current_cohorts.cohort_end_date_1), :]
+   
     elseif includeTreatments == "endDate"
-        #println("Applying endDate filtering")
-        #println("periodPriorToIndex: ", periodPriorToIndex)
-        #println("cohort_start_date_1: ", current_cohorts.cohort_start_date_1)
-        #println("cohort_end_date: ", current_cohorts.cohort_end_date)
-        #println("cohort_end_date_1: ", current_cohorts.cohort_end_date_1)
-
         current_cohorts = current_cohorts[
             (current_cohorts.cohort_start_date_1 .- (periodPriorToIndex) .<= current_cohorts.cohort_end_date) .& 
             (current_cohorts.cohort_start_date .< current_cohorts.cohort_end_date_1), :]
         current_cohorts.cohort_start_date = max.(
             current_cohorts.cohort_start_date_1 .- (periodPriorToIndex), current_cohorts.cohort_start_date)
     else
-        #println("includeTreatments input incorrect, returning all event cohorts ('includeTreatments')")
         current_cohorts = current_cohorts[
             (current_cohorts.cohort_start_date_1 .- (periodPriorToIndex) .<= current_cohorts.cohort_start_date) .& 
             (current_cohorts.cohort_start_date .< current_cohorts.cohort_end_date_1), :]
     end
-
-    #println("\nFiltered DataFrame after applying includeTreatments:")
-    #println(current_cohorts)
 
     # Calculate duration and gap same
     sort!(current_cohorts, [:cohort_start_date, :cohort_end_date])
